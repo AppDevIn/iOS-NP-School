805128a58e282315d320d38ee18420eb82e766c5
AppDevIn <teamprojectlive@gmail.com>
2020-12-06 13:03:28 +0800

    Scan the codee

--- QRcode/QRcode/Controller/GenerateQRCodeController.swift
+++ QRcode/QRcode/Controller/GenerateQRCodeController.swift
@@ -8,15 +8,20 @@
 import Foundation
 import UIKit
 import AVFoundation
+import QRCoder
 
-
-class GenerateQRCodeController: UIViewController {
+class GenerateQRCodeController: QRCodeScannerViewController {
     
-    
-    
-    override func viewDidLoad() {
-        
+    override func processQRCodeContent(qrCodeContent: String) -> Bool {
+        print(qrCodeContent)
+        dismiss(animated: true, completion: nil)
+        return true
     }
+    
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
     
     
     
