eed0500daf142c00fc4218fde1987b8990c4f1e5
AppDevIn <teamprojectlive@gmail.com>
2020-12-10 10:20:07 +0800

    Show error message

--- revision_paper_01/revision_paper_01/Controller/AddRecipeViewController.swift
+++ revision_paper_01/revision_paper_01/Controller/AddRecipeViewController.swift
@@ -17,9 +17,47 @@ class AddRecipeViewController: UIViewController {
     }
     
     @IBAction func addRecipe(_ sender: Any) {
-    //Get One By One with Tag
-      if let txtField1 = self.view.viewWithTag(1) as? UITextField {
-          print(txtField1.text!)
-      }
+        
+        var count:Int = 0
+        if let time = txt_time.text, let title = txt_title.text{
+            if !time.isEmpty && !title.isEmpty {
+                for index in 1...5 {
+                    //Get One By One with Tag
+                    if let txtField1 = self.view.viewWithTag(index) as? UITextField {
+                        
+                        if let text = txtField1.text{
+                            if !text.isEmpty {
+                                print(txtField1.text!)
+                                count += 1
+                            }
+                        }
+                        
+                    }
+                }
+            } else {
+                alert("Please populate the title and preration time")
+            }
+        }
+        
+        if(count < 1){
+            alert("Need at least one ingredient")
+            return
+        }
+        
+        
+        
     }
+    
+    func alert(_ message:String){
+        print("Alert: \(message)")
+        let alert = UIAlertController(title: "Empty field", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Noted", style: UIAlertAction.Style.default){_ in
+            self.dismiss(animated: true, completion: nil)
+        })
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
