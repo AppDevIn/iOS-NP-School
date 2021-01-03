c6a89fce9c5f2c273d6ecbe63a4bf5d3bf7ce7c4
AppDevIn <teamprojectlive@gmail.com>
2021-01-03 20:22:53 +0800

    Using forward geocoding to get the lat and lng of ngee ann

--- Telegramme/Telegramme/Controller/MapController.swift
+++ Telegramme/Telegramme/Controller/MapController.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 import MapKit
 
+
 class MapController : UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
@@ -16,22 +17,34 @@ class MapController : UIViewController {
     override func viewDidLoad() {
         print("Map view loaded")
         
-        //Set the location needed to be annoated
-        let location = CLLocationCoordinate2D(latitude: 1.332064,
-                                              longitude: 103.7743581)
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString("535 Clementi Road Singapore 599489") { (p, e) in
+            let lat = String(
+                    format: "Lat: %3.12f", p![0].location!.coordinate.latitude)
+                let long = String(
+                    format: "Long: %3.12f", p![0].location!.coordinate.longitude)
+
+                print("\(lat), \(long)")
+            
+            //Set the location needed to be annoated
+            let location = p![0].location!.coordinate
+            
+            //Zoom into that location
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location, span: span)
+            self.mapView.setRegion(region, animated: true)
+            
+            // Set the annoate
+            let annotation = MKPointAnnotation()
+            
+            annotation.coordinate = location
+            annotation.title = "Ngee Ann Polytechnic"
+            annotation.subtitle = "Schoo of ICT"
+            self.mapView.addAnnotation(annotation)
+
+        }
         
-        //Zoom into that location
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
-        
-        // Set the annoate 
-        let annotation = MKPointAnnotation()
-        
-        annotation.coordinate = location
-        annotation.title = "Ngee Ann Polytechnic"
-        annotation.subtitle = "Schoo of ICT"
-        mapView.addAnnotation(annotation)
+ 
         
     }
 }
